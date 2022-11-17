37e9bd441fa41f237194c634363692de1def104a
frankdonny <arkohfrank1@gmail.com>
2022-11-17 21:49:05 +0000

    0x05. AirBnB clone - RESTful API

---
+++ api/v1/app.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+from models import storage
+from os import getenv
+from flask import Flask, jsonify
+from models import storage
+from api.v1.views import app_views
+
+app = Flask(__init__)
+app.register_blueprint(app_views)
+
+
+@app.errorhandler(404)
+def error_handler(e):
+    """error handler method"""
+    return jsonify({"error": "Not found"})
+
+
+@app.teardown_appcontext
+def teardown(exception):
+    """method to handle remove of a session"""
+    storage.close()
+
+
+if __name__ == "__main__":
+    ht = None
+    pt = None
+    if getenv("HBNB_API_HOST"):
+        ht = getenv("HBNB_API_HOST")
+    else:
+        ht = "0.0.0.0"
+
+    if getenv("HBNB_API_PORT"):
+        pt = getenv("HBNB_API_PORT")
+    else:
+        pt = 5000
+    app.run(host=ht, port=pt, threaded=True, debug=True)
---
+++ api/v1/views/index.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+from flask import jsonify
+from api.v1.views import app_views
+from models import storage
+# from os import getenv
+
+
+@app_views.route("/status")
+def status():
+    return jsonify({"status": "OK"})
+
+
+@app_views.route("/stats")
+def stat():
+    """method to retrieve number of objects by type"""
+    return jsonify({
+        "amenities": storage.count("Amenity"),
+        "cities": storage.count("City"),
+        "places": storage.count("Place"),
+        "reviews": storage.count("Review"),
+        "states": storage.count("State"),
+        "users": storage.count("User")
+    })
