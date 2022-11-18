c0aa4a82463325c675ceb11eaedc94f49c42b461
FrankDonny <101371802+FrankDonny@users.noreply.github.com>
2022-11-18 20:01:30 +0000

    0x05. AirBnB clone - RESTful API

--- api/v1/app.py
+++ api/v1/app.py
@@ -12,7 +12,7 @@ app.register_blueprint(app_views)
 @app.errorhandler(404)
 def error_handler(e):
     """error handler method"""
-    return jsonify({"error": "Not found"})
+    return jsonify({"error": "Not found"}), 404
 
 
 @app.teardown_appcontext
